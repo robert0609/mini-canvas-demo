42f78fa882e5aa83fbb50494f7506d4725df9668
robert <robert0609@163.com>
2024-08-14 16:48:13 +0800

    feat: add cax

--- src/wxcomponents/map/index.js
+++ src/wxcomponents/map/index.js
@@ -7,21 +7,49 @@ Component({
       //比 web 里使用 cax 多传递 this，this 代表 Page 或 Component 的实例
       const windowInfo = wx.getWindowInfo()
       const stage = new cax.Stage(windowInfo.windowWidth, windowInfo.windowHeight, 'myCanvas', this);
-      const rect = new cax.Rect(100, 100, {
-        fillStyle: 'black'
+
+      const bitmap = new cax.Bitmap('https://h5-static-test.xunbaoji.net.cn/batman.jpg', () => {
+        stage.update();
+      })
+      stage.add(bitmap);
+
+      stage.on('touchstart', (e) => {
+        console.log('touchstart', e)
       })
       
-      rect.originX = 50
-      rect.originY = 50
-      rect.x = 100
-      rect.y = 100
-      rect.rotation = 30
+      stage.on('touchmove', (e) => {
+        console.log('touchmove', e)
 
-      rect.on('tap', () => {
-        console.log('tap')
+        bitmap.x = e.stageX;
+        bitmap.y = e.stageY;
+        stage.update()
+      })
+      
+      stage.on('touchend', (e) => {
+        console.log('touchend', e, bitmap, stage)
+
+        // stage.scaleX -= 0.1;
+        // stage.scaleY -= 0.1;
+        // stage.update()
       })
 
-      stage.add(rect)
+      // const rect = new cax.Rect(100, 100, {
+      //   fillStyle: 'black'
+      // })
+      
+      // rect.originX = 50
+      // rect.originY = 50
+      // rect.x = 100
+      // rect.y = 100
+      // rect.rotation = 30
+
+      // rect.on('tap', () => {
+      //   console.log('tap')
+      // })
+
+      // stage.add(rect)
+
+
       stage.update()
     },
     detached: function() {
